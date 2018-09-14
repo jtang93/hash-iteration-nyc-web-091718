ae71edaaa131fed3f8db0bf45d8057e633ead265
jtang93 <tang.jonathan93@gmail.com>
2018-09-14 09:39:54 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,12 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suit == :siot_a && name.start_with?("A")
+      winner = name
+    end
+  end
   
+  winner
 end
